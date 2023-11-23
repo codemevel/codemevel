468ab46e8b1544538df9576dba4d1934169dfb02
Amit212119 <amgiri1010@gmail.com>
2023-11-23 20:20:56 +0530

    fix typo

--- src/app/blog/List.tsx
+++ src/app/blog/List.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
 
-import SubTitle from '../_ui/SubTitle'
-import Title from '../_ui/Title'
-
 function List() {
   const blogData = [
     {
@@ -49,9 +46,9 @@ function List() {
 
   return (
     <ul className="flex grid grid-cols-12 justify-center items-center gap-5">
-      {blogData.map((data, i) => (
-        <li key={i} className="border  h-96 w-96 p-3 col-span-4">
-          <img src={data.imgSource} alt={`Blog ${i}`} />
+      {blogData.map((data) => (
+        <li key={data.heading} className="border  h-96 w-96 p-3 col-span-4">
+          <img src={data.imgSource} alt={data.heading} />
           <h1 className="text-2xl py-10">{data.heading}</h1>
           <p className="text-xl">{data.content}</p>
         </li>
