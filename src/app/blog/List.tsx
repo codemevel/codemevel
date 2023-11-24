373d1e2c59b01719e465b45b00bfff2ae480f477
Amit212119 <amgiri1010@gmail.com>
2023-11-24 11:01:55 +0530

    update blog

--- src/app/blog/List.tsx
+++ src/app/blog/List.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import Button from '../_ui/Button'
+
 function List() {
   const blogData = [
     {
@@ -45,12 +47,27 @@ function List() {
   ]
 
   return (
-    <ul className="flex grid grid-cols-12 justify-center items-center gap-5">
-      {blogData.map((data) => (
-        <li key={data.heading} className="border  h-96 w-96 p-3 col-span-4">
-          <img src={data.imgSource} alt={data.heading} />
-          <h1 className="text-2xl py-10">{data.heading}</h1>
-          <p className="text-xl">{data.content}</p>
+    <ul className="gap-5 grid  dot-matrix  grid-cols-4 md:grid-cols-8 px-5 relative  pb-10">
+      {blogData.map((service, index) => (
+        <li
+          key={service.heading}
+          className="mb-2 p-5 flex flex-col justify-between relative hover:scale-[.99]  col-span-4  bg-white/50 dark:bg-black/50  border-[0.5px] border-white/30 rounded-sm dark:text-white/50 text-black/50 hover:bg-white/10"
+        >
+          <div className="h-10 w-10 border-t-[0.5px] absolute top-1 left-1 border-l-[0.5px] opacity-30" />
+          <div className="h-10 w-10 border-b-[0.5px] absolute bottom-1 right-1 border-r-[0.5px] opacity-30" />
+
+          <div className="pb-5 slashed-zero text-primary font-mono">
+            <p> 0{index + 1}</p>
+          </div>
+          <h3 className="text-2xl  py-5 font-semibold text-white">
+            {service.heading}
+            <br />
+          </h3>
+          <p className="mb-5"> {service.content}</p>
+          <Button
+            title="Read more ->"
+            ariaLabel={`Read more about ${service.heading}`}
+          />
         </li>
       ))}
     </ul>
